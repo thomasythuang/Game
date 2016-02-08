5f49453b40881bdafd50af475183a9dd938fe0ef
Kaitlyn Macintyre <Kaitlyn Macintyre>
2016-02-07 22:30:21 -0600

    Added move and jump capabilities to character

---
+++ Assets/CharScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	//public Vector2 JumpVelocity;
+	//public Vector2 RunVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharScript : MonoBehaviour {
+
+	public Vector2 JumpVelocity;
+	public Vector2 RunVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			this.GetComponent<Rigidbody2D>().AddForce(JumpVelocity, ForceMode2D.Impulse);
+		}
+
+		if (Input.GetButtonDown("Horizontal")) {
+			this.GetComponent<Rigidbody2D>().AddForce(RunVelocity, ForceMode2D.Force);
+		}
+	}
+}

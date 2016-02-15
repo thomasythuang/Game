6ce4ee437e2e8ad805d407210d0bbe8e63c14f0b
Thomas Huang <thomas.yt.huang@gmail.com>
2016-02-15 17:32:37 -0600

    primitive level switching on reaching exit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    private bool characterInZone;
+
+    public string LevelToLoad;
+
+	// Use this for initialization
+	void Start () {
+        characterInZone = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.W) && characterInZone)
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            characterInZone = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            characterInZone = false;
+        }
+    }
+}

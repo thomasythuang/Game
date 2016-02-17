f38ed2dfc9fb605774266db53fe90060661dd81b
Thomas Huang <thomas.yt.huang@gmail.com>
2016-02-17 00:21:28 -0600

    add primitive level select screen

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+    // This is a pretty hacky way- will look later for
+    // a way to load levels dynamically
+
+    public string level0;
+    public string level1;
+    public string level2;
+    public string level3;
+
+    public void LoadLevel0()
+    {
+        SceneManager.LoadScene(level0);
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(level1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(level2);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(level3);
+    }
+}

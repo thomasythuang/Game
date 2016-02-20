f54edad63147c466b371550e0d8c5b08a8f74a74
Thomas Huang <thomas.yt.huang@gmail.com>
2016-02-19 22:29:04 -0600

    modify level loading to make it dynamic instead of hardcoded

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -2,33 +2,12 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class LevelSelect : MonoBehaviour {
+public class LevelSelect : MonoBehaviour
+{
 
-    // This is a pretty hacky way- will look later for
-    // a way to load levels dynamically
-
-    public string level0;
-    public string level1;
-    public string level2;
-    public string level3;
-
-    public void LoadLevel0()
+    // For each level button, specify the level number in the onClick section of the Unity Editor
+    public void LoadLevel(int num)
     {
-        SceneManager.LoadScene(level0);
-    }
-
-    public void LoadLevel1()
-    {
-        SceneManager.LoadScene(level1);
-    }
-
-    public void LoadLevel2()
-    {
-        SceneManager.LoadScene(level2);
-    }
-
-    public void LoadLevel3()
-    {
-        SceneManager.LoadScene(level3);
+        SceneManager.LoadScene("Level " + num.ToString());
     }
 }
